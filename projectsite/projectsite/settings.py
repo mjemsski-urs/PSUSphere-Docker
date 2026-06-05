c9fa98e6b546a3c38a998b77ee3146d0f471ca2e
aeprnts <cessywp16@gmail.com>
2026-06-05 11:40:56 +0800

    ADD DOCKER SET UP

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ccmh^*lj&x$@_drzv+!=zoaq(z0=+uz_716oc1f)sz^lar8(**
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'psusphere.pythonanywhere.com', 'mjemsski.pythonanywhere.com', '127.0.0.1' ]
+ALLOWED_HOSTS = [ 'psusphere.pythonanywhere.com', 'mjemsski.pythonanywhere.com', '127.0.0.1','localhost' ]
 
 # Application definition
 
